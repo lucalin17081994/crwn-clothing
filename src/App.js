ed64556eb0bea04ae611c6802d76ee617d7f9c25
agaham <agahamsc@live.com>
2021-07-28 00:50:22 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component'
-
+import {Switch, Route} from 'react-router-dom'
 function App() {
   return (
-    <Homepage></Homepage>
+    <div>
+      <Switch>
+        <Route exact path='/' component={Homepage}></Route>
+        <Route path='/shop/hats' >HatsPage</Route>
+      </Switch>
+    </div>
+    
   );
 }
 
