8e1035617e19edc4281a1ed1a653e1437d32ac42
agaham <agahamsc@live.com>
2021-07-28 20:01:44 +0200

    added items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 import {Switch, Route} from 'react-router-dom'
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage}></Route>
-        <Route path='/shop/hats' >HatsPage</Route>
+        <Route path='/shop' component={ShopPage}></Route>
       </Switch>
     </div>
     
---
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './preview-collection.styles.scss'
+import CollectionItem from '../collection-item/collection-item.component'
+
+const PreviewCollection=({title,items})=>{
+  return (
+    <div className='collection-preview'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <div className='preview'>
+        {
+          items
+          .filter((item,idx)=>idx<4)
+          .map(({id, ...itemProps})=>(
+            <CollectionItem key={id}{...itemProps}></CollectionItem>
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default PreviewCollection;
